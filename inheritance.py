d0b77ed638dea75af69a91c4d4c027761d76c990
shaked20-meet <shaked20@meet.mit.edu>
2018-11-21 19:02:50 +0200

    lab 6 colors

--- inheritance.py
+++ inheritance.py
@@ -3,18 +3,20 @@ from turtle import Turtle
 turtle.colormode(255)
 import random
 class Square(Turtle):
-	def __init__(self, shapesize, shape, color):
+	def __init__(self, shapesize, shape):
 		Turtle.__init__(self)
 		self.shapesize(shapesize)
 		self.shape(shape)
-		self.color(r+b+g)
+		
 		
 	def random_color1(self):
 		r= random.randint(0,255)
 		g= random.randint(0,255)
 		b= random.randint(0,255)
-		self.color(r+b+g)
+		self.color(r,g,b)
 		
+
 s = Square(8,"square")
 s.random_color1()
 turtle.mainloop()
+
