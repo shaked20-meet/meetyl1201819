b7ae12a97e98307f5e3818731265207a14dbc2f3
shaked20-meet <shaked20@meet.mit.edu>
2019-01-24 18:01:39 +0200

    my cs project

--- Agario.py
+++ Agario.py
@@ -1,6 +1,7 @@
 import turtle
 import time
 import random
+import math
 import CS_final_project
 from CS_final_project import Ball
 
@@ -14,7 +15,7 @@ SCREEN_HEIGHT = turtle.getcanvas().winfo_height()/2
 NUMBER_OF_BALLS = 5
 
 MINIMUM_BALL_RADIUS = 10
-MAXIMUM_BALL_RADIUS = 100
+MAXIMUM_BALL_RADIUS = 101
 
 MINIMUM_BALL_DX = -5
 MAXIMUM_BALL_DX = 5
@@ -25,29 +26,82 @@ MAXIMUM_BALL_DY = 5
 BALLS = []
 
 ####################PART 0########################
-
+#hoped@mit.edu
 
 MY_BALL = Ball(0,0,1,1,10,"red")
+def balls_creating():
+###################################PART 3 - SMALL BALL##################################
+	X_axis_speed = random.randint(-10,10)
 
+	while X_axis_speed == 0:        #====>Makes sure the speed isn't 0 
+		X_axis_speed = random.randint(-10,10)
 
-for i in range(NUMBER_OF_BALLS):
-	x = random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS,SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
-	y = random.randint(-SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS, SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
-	dx = random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
-	dy = random.randint(MINIMUM_BALL_DY, MAXIMUM_BALL_DY)
-	radius = random.randint(MINIMUM_BALL_RADIUS, MAXIMUM_BALL_RADIUS)
-	color = (random.random(), random.random(), random.random())
-	ball = Ball(x,y,dx, dy, radius, color)
-	BALLS.append(ball)
-turtle.update()
+	Y_axis_speed = random.randint(-10,10)
+	while Y_axis_speed == 0:
+		Y_axis_speed = random.randint(-10,10)
+	#############################################################################
+
+	for i in range (NUMBER_OF_BALLS): #==========> Creates new balls according to the list
+		X_coordinate = random.randint(-200, 200)
+		Y_coordinate = random.randint(-200, 200)
+		radius_small = random.randint(10,150)
+		color = color = (random.random(), random.random(), random.random())
+		X_axis_speed = random.randint(-10,10)
+		##============> Above: small ball variables, below: large ball varoables
+		x = random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS,SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
+		y = random.randint(-SCREEN_HEIGHT + MAXIMUM_BALL_RADIUS, SCREEN_HEIGHT - MAXIMUM_BALL_RADIUS)
+		dx = random.randint(MINIMUM_BALL_DX , MAXIMUM_BALL_DX)
+		dy = random.randint(MINIMUM_BALL_DY, MAXIMUM_BALL_DY)
+		radius = random.randint(MINIMUM_BALL_RADIUS, MAXIMUM_BALL_RADIUS)
+		color = (random.random(), random.random(), random.random())
+		big_ball = Ball(x, y, dx, dy, radius, color)
+		small_ball = Ball(X_coordinate, Y_coordinate, X_axis_speed, Y_axis_speed, radius_small, color)
+		BALLS.append(big_ball)#=====> appending balls to the BALLS list
+		BALLS.append(small_ball)
+	turtle.update()
 
 ################PART 1##########################
 def move_all_balls():
 	for ball in (BALLS):
 		ball.move_ball(SCREEN_WIDTH, SCREEN_HEIGHT)
-
+###############PART 2##########################
+def check_collisions(ball_a, ball_b):
+	if ball_a.radius == ball_b.radius:
+		return False
+	collide_dist = ball_a.radius + ball_b.radius
+	distance = math.sqrt (math.pow(ball_a.xcor() - ball_b.xcor(),2) + math.pow(ball_a.ycor() - ball_b.ycor(),2))
+	if distance + 10 <= collide_dist:
+		return True
+	else:
+		return False
+############PART 3#############################
+def check_all_balls_collision():
+	for ball_a in (BALLS):
+		for ball_b in (BALLS):
+			check_collisions(ball_a, ball_b)
+			if True:
+				ball_a_radius = ball_a.radius
+				ball_b_radius = ball_b.radius
+			
+############PART 4##################################
+def check_myball_collision():
+	for ball in (BALLS):
+		collide_dist_2 = MY_BALL.radius + ball.radius
+		distance_2 = math.sqrt (math.pow(MY_BALL.xcor() - ball.xcor(),2) + math.pow(MY_BALL.ycor() - ball.ycor(),2))
+		if distance_2 + 10 <= collide_dist_2:
+			MY_BALL_R = MY_BALL.radius
+			COLLIDE_BALL_R = ball.radius
+			if MY_BALL_R < COLLIDE_BALL_R:
+				return False
+			else:
+				balls_creating()
+				MY_BALL_R += COLLIDE_BALL_R
+balls_creating()
 while RUNING:
 	move_all_balls()
+	check_all_balls_collision()
+	check_myball_collision()
+
 	turtle.update()
 
 
--- try.py
+++ try.py
@@ -1,9 +1,6 @@
 import turtle
-x=0
-y=40
-turtle.penup()
-turtle.goto(x,y)
-turtle.pendown()
-turtle.circle(20)
+turtle.speed(-100)
+turtle.shape("circle")
+turtle.goto(500,100)
 
 turtle.mainloop()
